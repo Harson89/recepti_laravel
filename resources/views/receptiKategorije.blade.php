a33cbac7b6e30ca95b9e46e3fdf6ea90f4ae9280
Harson89 <harson8_harson9@hotmail.com>
2018-11-13 13:19:47 +0100

    Napravljen poseban ispis kategorija sa id

--- resources/views/receptiKategorije.blade.php
+++ resources/views/receptiKategorije.blade.php
@@ -1,6 +1,32 @@
 @extends('layouts.app')
 @section('content')
 
+<?php
+  $kategorije = App\kategorije2::all();
+?>
 
+<div class="jumbotron">
+
+<h1> Kategorije </h1>
+
+<br> <br>
+
+<div>
+
+@foreach ($kategorije as $kategorija)
+
+<a href="/receptiKategorije/{{$kategorija->id}}">
+    <button class="btn btn-primary">
+        <p> {{$kategorija->naziv_kategorije}} </p>
+    </button>
+</a>
+<br>
+<br>
+
+@endforeach
+
+</div>
+
+</div>
 
 @endsection
