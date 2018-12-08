df0cf7be2b5cad5d9ff90f884f959e26a3acf4b4
Harson89 <harson8_harson9@hotmail.com>
2018-12-08 18:37:53 +0100

    Prikaz popravljen

--- resources/views/kuharovi_recepti.blade.php
+++ resources/views/kuharovi_recepti.blade.php
@@ -1,30 +1,35 @@
 @extends('layouts.app')
 @section('content')
 
-
-
 <table class="table">
     <thead>
       <tr>
-        <th scope="col">#</th>
+        <th scope="col">Otvori</th>
         <th scope="col">Naziv recepta</th>
-        <th scope="col">Priprema</th>
+        <th scope="col">Kuhar</th>
         <th scope="col">Slika</th>
 
       </tr>
     </thead>
     <tbody>
          @foreach ($recepti as $recepat)
-
       <tr>
-      <th scope="row">{{$recepat->id}}</th>
-
+          <?php
+            $imeKuhara= App\User::where('id', $recepat->user_id)->get();
+            foreach($imeKuhara as $kuhar){
+                $ime = $kuhar->name;
+            }
+          ?>
+      <th scope="row">
+        <a href="/pojedinacanRecept/{{$zaPrikaz = $recepat->id}}"> <button class="btn btn-outline-secondary"> Otvori </button> </a>  </td>
+      </th>
         <td>{{$recepat->naziv_recepta}}</td>
-        <td style="width: 35%; word-break:break-all;">{{$recepat->priprema}}</td>
-      <td> <img src="{{$recepat->slika}}" class="img-thumbnail" alt="Cinque Terre" width="500px" height="500px"> </td>
-
+        <td><a href="/kuharoviRecepti/{{$id_kuhara = $kuhar->id}}">{{$ime}}</a></td>
+      <td> <img src="{{$recepat->slika}}" class="img-fluid" alt="Responsive image" width="500px" height="500px" > </td>
       </tr>
 
 @endforeach
 </table>
+
+
 @endsection
--- resources/views/receptiKategorije.blade.php
+++ resources/views/receptiKategorije.blade.php
@@ -8,7 +8,7 @@ $trenutnaKategorija = $receptiKategorije;
 <table class="table">
         <thead>
           <tr>
-            <th scope="col">#</th>
+
             <th scope="col">Naziv recepta</th>
             <th scope="col">Kuhar</th>
             <th scope="col">Priprema</th>
@@ -26,10 +26,12 @@ $trenutnaKategorija = $receptiKategorije;
                     $ime = $kuhar->name;
                 }
              ?>
-          <th scope="row">{{$receptiTe->id}}</th>
+
+          <th scope="row">
+            <a href="/pojedinacanRecept/{{$zaPrikaz = $receptiTe->id}}"> <button class="btn btn-outline-secondary"> Otvori </button> </a>  </td>
+          </th>
             <td>{{$receptiTe->naziv_recepta}}</td>
             <td>{{$ime}}</td>
-            <td style="width: 35%; word-break:break-all;">{{$receptiTe->priprema}}</td>
             <td> <img src="{{$receptiTe->slika}}" class="img-fluid" alt="Responsive image" width="500px" height="500px" > </td>
         </tr>
           @endforeach
